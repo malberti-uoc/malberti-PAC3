33e5c1f33a212b3e129f62bcced80c36af56f704
malberti-UOC <malberti@uoc.edu>
2020-12-28 19:56:39 +0100

    20201228-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
   purge: ["./src/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+     'body': ['Montserrat', 'sans-serif'],
+    },
     extend: {
       colors: {
         teal: colors.teal
